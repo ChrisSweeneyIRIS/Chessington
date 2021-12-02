1c958a6ec108ce90bfee730210ebc3953ebc7579
ChrisSweeneyIRIS <93913592+ChrisSweeneyIRIS@users.noreply.github.com>
2021-12-02 14:52:32 +0000

    Red-06
    Knight Moves

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,26 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            {
+                var currentSquare = board.FindPiece(this);
+
+                List<Square> moves = new List<Square>();
+
+                int minMove = -1;
+                int maxMove = 8;
+                
+                // 2D Array for Potential Moves
+                int[,] knightMoves = { { -2, 1 }, { -1, 2 }, { 1, 2 }, { 2, 1 }, { 2, -1 }, { 1, -2 }, { -1, -2 }, { -2, -1 } };
+
+                for (int i = 0; i < 8; i++)
+                {
+                    if (currentSquare.Row + knightMoves[i, 0] < maxMove && currentSquare.Row + knightMoves[i, 0] > minMove && currentSquare.Row + knightMoves[i, 1] < maxMove && currentSquare.Row + knightMoves[i, 1] > minMove)
+                    {
+                        moves.Add(new Square(currentSquare.Row + knightMoves[i, 0], currentSquare.Col + knightMoves[i, 1]));
+                    }
+                }
+                return moves;
+            }
         }
     }
 }
