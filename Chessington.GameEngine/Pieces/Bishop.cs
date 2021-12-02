407c1b0428d54b0c142e25f040f1672eee67e778
ChrisSweeneyIRIS <93913592+ChrisSweeneyIRIS@users.noreply.github.com>
2021-12-02 14:16:08 +0000

    Red-04
    Bishop now works

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,35 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+
+            List<Square> moves = new List<Square>();
+            
+            int minMoves = -1;
+            int maxMoves = 8;
+            
+
+            for (int col = currentSquare.Col + 1, row = currentSquare.Row + 1; col < maxMoves && row < maxMoves; row++, col++)
+            {
+                moves.Add(new Square(row, col));
+            }
+
+            for (int col = currentSquare.Col - 1, row = currentSquare.Row + 1; col > minMoves && row < maxMoves; row++, col--)
+            {
+                moves.Add(new Square(row, col));
+            }
+
+            for (int col = currentSquare.Col - 1, row = currentSquare.Row - 1; col > minMoves && row > minMoves; row--, col--)
+            {
+                moves.Add(new Square(row, col));
+            }
+
+            for (int col = currentSquare.Col + 1, row = currentSquare.Row - 1; col < maxMoves && row > minMoves; row--, col++)
+            {
+                moves.Add(new Square(row, col));
+            }
+
+            return moves;
         }
     }
 }
